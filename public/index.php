26fd7dca76ae07f55cc540330d01e12db2f6f099
Prashant27011998 <prashantjha9610533986@gmail.com>
2020-06-07 18:19:44 +0530

    Heroku

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
     <head>
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet"> 
         <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet"> 
-        <link href="./Laravel/public/css/app.css" rel="stylesheet"> 
+        <link href="./css/app.css" rel="stylesheet"> 
 
     </head>
 <body>
